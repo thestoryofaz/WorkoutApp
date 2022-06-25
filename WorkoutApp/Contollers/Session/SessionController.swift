4585adb47a3667cf4c750452da0eb2debae712df
aiv <96691834+aiv26@users.noreply.github.com>
2022-06-25 23:35:32 +0300

    add view on session

--- WorkoutApp/Contollers/Session/SessionController.swift
+++ WorkoutApp/Contollers/Session/SessionController.swift
@@ -1,14 +1,28 @@
 import UIKit
 
-class SessionController: BaseContoller {}
+class SessionController: BaseContoller {
+    private let timerView: BaseInfoView = {
+        let view = BaseInfoView(with: "Test", aligment: .center)
+        return view
+    }()
+}
 
 extension SessionController {
     override func setupViews() {
          super.setupViews()
+        
+        view.setupView(timerView)
     }
     
     override func constraintViews() {
         super.constraintViews()
+        
+        NSLayoutConstraint.activate([
+            timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
+            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            timerView.heightAnchor.constraint(equalToConstant: 300)
+        ])
     }
     
     override func configureAppearance() {
